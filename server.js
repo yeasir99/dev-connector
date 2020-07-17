521f7a00e0b47215677bf8870eb8e11f317bb0e9
Yeasir Ahamed <yahamed007@gmail.com>
2020-07-17 14:25:51 +0600

    define routes

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const app = express();
 //Connect Database
 connectDB();
 
-app.get("/", (req, res) => {
-    res.send(`Hello World`);
-});
+//Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/profile', require('./routes/profile'));
+app.use('api/posts', require('./routes/posts'));
+
 
 const PORT = process.env.PORT || 5000;
 
