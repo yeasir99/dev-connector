dd8b21348033ee64af711c8c9a3e2bd1c996eaa2
Yeasir Ahamed <yahamed007@gmail.com>
2020-07-21 04:56:33 +0600

    add alert component and manage alert

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -25,7 +28,7 @@ const Register = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Password not match");
+      setAlert("Password not match", "danger");
     } else {
       console.log("Success");
     }
@@ -91,4 +94,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
---
+++ client/src/reducers/alert.js
@@ -0,0 +1,14 @@
+import * as ACTIONS from "../actions/types";
+const initialState = [];
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case ACTIONS.SET_ALERT:
+      return [...state, payload];
+    case ACTIONS.REMOVE_ALERT:
+      return state.filter((alert) => alert.id !== payload);
+    default:
+      return state;
+  }
+}
