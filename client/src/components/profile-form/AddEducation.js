e71ee8c8f4be9e424258ade747b9f12980d50df8
Yeasir Ahamed <yahamed007@gmail.com>
2020-07-24 23:16:18 +0600

    addressing some console warnings

--- client/src/components/profile-form/AddEducation.js
+++ client/src/components/profile-form/AddEducation.js
@@ -132,4 +132,4 @@ const AddEducation = ({ addEducation, history }) => {
 AddEducation.propTypes = {
   addEducation: PropTypes.func.isRequired,
 };
-export default connect(null, { addEducation })(AddEducation);
+export default connect(null, { addEducation })(withRouter(AddEducation));
