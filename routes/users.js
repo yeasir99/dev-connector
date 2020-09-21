aae7c8490c1103a1b1f9e5aba41463b64317f0f4
Yeasir Ahamed <yahamed007@gmail.com>
2020-09-21 23:59:53 +0600

    update users controller

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken');
-const config = require('config');
 const {
-    check,
-    validationResult
+    check
 } = require("express-validator");
-
-const User = require("../models/User");
+const {
+    register
+} = require('../controller/users');
 
 // @route      POST api/users
 // @desc       Register User
@@ -26,80 +22,7 @@ router.post(
             min: 6,
         }),
     ],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array(),
-            });
-        }
-
-        const {
-            name,
-            email,
-            password
-        } = req.body;
-
-        try {
-            // check if user exists
-            let user = await User.findOne({
-                email,
-            });
-
-            if (user) {
-                return res.status(400).json({
-                    errors: [{
-                        msg: "User already exists",
-                    }, ],
-                });
-            }
-
-            // Get user gravatar
-            const avatar = gravatar.url(
-                email, {
-                    s: "200",
-                    r: "pg",
-                    d: "mm",
-                },
-                true
-            );
-
-            user = new User({
-                name,
-                email,
-                avatar,
-                password,
-            });
-
-
-            // Encrypt password
-
-            const salt = await bcrypt.genSalt(10);
-            user.password = await bcrypt.hash(password, salt);
-
-            await user.save();
-
-            // Return json webtoken
-
-            const payload = {
-                user: {
-                    id: user.id
-                }
-            }
-
-            jwt.sign(payload, config.get('jwtSecret'), {
-                expiresIn: 36000
-            }, (err, token) => {
-                if (err) throw err;
-                res.json({
-                    token
-                })
-            })
-        } catch (err) {
-            console.log(err.message);
-            res.status(500).send("Server Error");
-        }
-    }
+    register
 );
 
 module.exports = router;
