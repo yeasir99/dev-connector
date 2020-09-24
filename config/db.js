b7cd1f68a0a9c9b91c2ad409edadcfb72b608291
Yeasir Ahamed <yahamed007@gmail.com>
2020-09-24 21:09:50 +0600

    add some security

--- config/db.js
+++ config/db.js
@@ -4,19 +4,19 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        })
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
 
-        console.log(`Connected Database`);
-    } catch (err) {
-        console.error(err.message)
-        process.exit(1)
-    }
-}
+    console.log(`Connected Database`);
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = require("express");
+const path = require('path');
+const express = require('express');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 const connectDB = require('./config/db');
-const path = require('path')
 
 const app = express();
 
@@ -8,9 +13,26 @@ const app = express();
 connectDB();
 
 //init Midleware
-app.use(express.json({
-    extended: false
-}))
+app.use(express.json());
+
+// sanitize data
+app.use(mongoSanitize());
+
+// set security headers
+app.use(helmet());
+
+// prevent xss attacks
+app.use(xss());
+
+// Rate limiting
+const limitter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100,
+});
+app.use(limitter);
+
+// prevent http param polution
+app.use(hpp());
 
 //Define Routes
 app.use('/api/users', require('./routes/users'));
@@ -18,15 +40,14 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/profile', require('./routes/profile'));
 app.use('/api/posts', require('./routes/posts'));
 
-// Serve static assets in production 
+// Serve static assets in production
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
-
 
 const PORT = process.env.PORT || 5000;
 
