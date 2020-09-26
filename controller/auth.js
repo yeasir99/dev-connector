69ca9917e450a6283dd31640b7582b7bf96498c8
Yeasir Ahamed <yahamed007@gmail.com>
2020-09-26 10:14:45 +0600

    update auth controller and route

--- controller/auth.js
+++ controller/auth.js
@@ -1,79 +1,59 @@
+const asyncHandler = require('../middleware/asyncHandler');
+const ErrorResponse = require('../utils/errorResponse');
 const User = require('../models/User');
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-const {
-    validationResult
-} = require("express-validator");
-
-
 
 // @route      GET api/auth
-// @desc       Test route
-// @access     Public
+// @desc       Get user by token
+// @access     Private
 
-exports.getUser = async (req, res, next) => {
-    try {
-        const user = await User.findById(req.user.id).select('-password');
-        res.json(user)
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send(`Server error`);
+exports.getUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('-password');
+  res.json(user);
+});
+
+// @route    POST api/auth
+// @desc     Authenticate user & get token
+// @access   Public
+
+exports.userLogin = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorResponse(`Please include email and password`));
+  }
+
+  // find user by email
+  const user = await User.findOne({
+    email,
+  });
+
+  // send error if user not found
+  if (!user) {
+    return next(new ErrorResponse('Invalid Credential', 400));
+  }
+  // compare password
+  const isMatch = await bcrypt.compare(password, user.password);
+  // send error if password not match
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid Credential', 400));
+  }
+  // send token to the user
+  jwt.sign(
+    {
+      id: user.id,
+    },
+    config.get('jwtSecret'),
+    {
+      expiresIn: 36000,
+    },
+    (err, token) => {
+      if (err) throw err;
+      res.status(200).json({
+        token,
+      });
     }
-}
-
-exports.userLogin = async (req, res, next) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        return res.select(400).json({
-            errors: errors.array()
-        })
-    }
-
-    const {
-        email,
-        password
-    } = req.body;
-
-    try {
-        // find user by email 
-        const user = await User.findOne({
-            email
-        });
-
-        // send error if user not found 
-        if (!user) {
-            return res.status(400).json({
-                errors: [{
-                    msg: `Invalid Credential`
-                }]
-            })
-        }
-        // compare password 
-        const isMatch = await bcrypt.compare(password, user.password);
-        // send error if password not match
-        if (!isMatch) {
-            return res.status(400).json({
-                errors: [{
-                    msg: `Invalid Credential`
-                }]
-            })
-        }
-        // send token to the user 
-        jwt.sign({
-            id: user.id
-        }, config.get('jwtSecret'), {
-            expiresIn: 36000
-        }, (err, token) => {
-            if (err) throw err;
-            res.status(200).json({
-                token
-            })
-        })
-    } catch (err) {
-        console.error(err);
-        res.status(500).send(`server error`)
-    }
-
-}
+  );
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,8 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const {
-    check
-} = require("express-validator");
-const {
-    getUser,
-    userLogin
-} = require('../controller/auth');
+const { getUser, userLogin } = require('../controller/auth');
 
-router
-    .route('/')
-    .get(auth, getUser)
-    .post([check('email', 'Please include a valid email').isEmail(),
-        check('password', 'Password is required').exists()
-    ], userLogin)
-
+router.route('/').get(auth, getUser).post(userLogin);
 
 module.exports = router;
